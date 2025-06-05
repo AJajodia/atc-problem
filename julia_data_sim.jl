71611d2bd227073074f1ead368bac62062c05839
mariahgloehr <loehrm@carleton.edu>
2025-06-05 11:26:54 -0400

    creating airport and sector dictionaries

--- julia_data_sim.jl
+++ julia_data_sim.jl
@@ -2,5 +2,7 @@ using Random
 using DataFrames
 using CSV
 
-airport_sectors <- CSV.read("mariah_airport_sectors.csv") 
-timetable <- CSV.read("mariah_timetable.csv")
+airport_sectors = CSV.read("mariah_airport_sectors.csv", DataFrame) 
+timetable = CSV.read("mariah_timetable.csv", DataFrame)
+
+airport_dict = Dict(row.airport => (row.lat, row.long) for row in eachrow(airport_sectors))
