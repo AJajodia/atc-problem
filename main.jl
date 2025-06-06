0b85f4e1f4374c1e0cbdc231f6a3350a221020ae
Anu Jajodia <53088267+AJajodia@users.noreply.github.com>
2025-06-06 12:56:43 -0500

    ingesting data

--- main.jl
+++ main.jl
@@ -1,23 +1,48 @@
-using JuMP, GLPK, CSV
+using JuMP, GLPK, CSV, DataFrames
 
+sectors_df = DataFrame(CSV.File("mariah_airports.csv"))
 
-airports = CSV.read("airports.csv", DataFrame)
+airports_df = dropmissing(sectors_df)
+
+airports_list = airports_df.airport
+
+airports = Dict(sectors_df.airport[i] => [sectors_df[i, col] for col in 6:ncol(sectors_df)] for i in 1:nrow(sectors_df))
+
+sectors = Dict(sectors_df.sector[i] => sectors_df.sector_capacity[i] for i in 1:nrow(sectors_df))
+
+sectors_list = sectors_df.sector
+
+timetable_df = DataFrame(CSV.File("mariah_timetable.csv"))
+
+P = DataFrame(CSV.File("flight_paths.csv", header = false))
+
+println(airports)
 
 # Preparing an optimization model
 m = Model(GLPK.Optimizer)
 
-F = 20
-K = 4
-J = 10
+F = nrow(timetable_df)
+K = sum([!ismissing(sectors_df.airport[i]) for i in 1:nrow(sectors_df)])
+J = nrow(sectors_df)
+N = [sum([flights[j,i] != "0" for j in 1:nrow(P)]) for i in 1:ncol(P)]
 
-N = [1:20]
 
-T_max = 24
-T_min 
+T = [findmin(timetable_df.depart_time): findmax(timetable_df.arrival_time)]
 
-T = [1:20]
+function D(k, t)
+    airport = airports_list[k]
+    airports[airport][8]
+end
 
-P = [[1, 2, 3],[4, 5, 6]]
+function A(k, t)
+    airport = airports_list[k]
+    return airports[airport][9]
+end
+
+function S(j, t)
+    sector = sectors_list[j]
+    return sectors[sector]
+
 
 # Declaring variables
 @variable(m, w[1:F, 1:24, 1:max(N)])
@@ -27,7 +52,7 @@ P = [[1, 2, 3],[4, 5, 6]]
 
 # Adding constraints
 
-# aiport and sector capacity constraints
+# airport and sector capacity constraints
 for k in 1:K, t in 1:T
     @constraint(m, sum((w[f, t, 1] - w[f, t-1, 1]) for f in 1:F if P[k][1] == k) <= D[t][k])
     @constraint(m, sum((w[f, t, 1] - w[f, t-1, 1]) for f in 1:F if P[k][1] == k) <= A[t][k])
