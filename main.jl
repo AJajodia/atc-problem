682980691bfbc7908b5402517191fde5615db0b1
Anu Jajodia <53088267+AJajodia@users.noreply.github.com>
2025-06-08 13:33:47 -0500

    finished LP in Julia (issues: negative cost when air cost is higher than ground cost

--- main.jl
+++ main.jl
@@ -6,36 +6,39 @@ sectors_df = DataFrame(CSV.File("mariah_airport_sectors.csv"))
 
 airports_df = dropmissing(sectors_df)
 
-airports_list = airports_df.airport
+airports_list = string.(airports_df.airport)
 
-airports = Dict(sectors_df.airport[i] => [sectors_df[i, col] for col in 6:ncol(sectors_df)] for i in 1:nrow(sectors_df))
+airports = Dict(string.(sectors_df.airport[i]) => [sectors_df[i, col] for col in 6:ncol(sectors_df)] for i in 1:nrow(sectors_df))
 
-sectors = Dict(sectors_df.sector[i] => sectors_df.sector_capacity[i] for i in 1:nrow(sectors_df))
+sectors = Dict(string.(sectors_df.sector[i]) => sectors_df.sector_capacity[i] for i in 1:nrow(sectors_df))
 
-sectors_list = sectors_df.sector
+sectors_list = string.(sectors_df.sector)
 
-timetable_df = DataFrame(CSV.File("mariah_timetable.csv"))
+timetable_df = DataFrame(CSV.File("mariah_timetable.csv")), DataFrame(CSV.File("mariah_timetable.csv"))
 
 sector_lookup = Dict(sectors_df.sector[i] => i for i in 1:nrow(sectors_df))
 
 airport_lookup = Dict(sectors_df.airport[i] => i for i in 1:nrow(sectors_df))
 
-buffer_time = 10
+l = DataFrame(CSV.File("min_times_anu.csv", header = false))
+
+start_df = DataFrame(CSV.File("flight_min_times_anu.csv", header = false)), DataFrame(CSV.File("flight_min_times_anu.csv", header = false))
+
+buffer_time = 1
 
 
-P = DataFrame(CSV.File("flight_paths_anu.csv", header = false))
+P = DataFrame(CSV.File("flight_paths_anu.csv", header = false)), DataFrame(CSV.File("flight_paths_anu.csv", header = false))
 
-println(P)
 # Preparing an optimization model
 m = Model(GLPK.Optimizer)
 
 F = nrow(timetable_df)
 K = nrow(airports_df)
 J = nrow(sectors_df)
-N = [sum([flights[j,i] != "0" for j in 1:nrow(P)]) for i in 1:ncol(P)]
+N = [sum([P[j,i] != "0" for j in 1:nrow(P)]) for i in 1:ncol(P)]
 
 
-T = [minimum(timetable_df.depart_time): maximum(timetable_df.arrival_time)]
+T = 100
 
 c = [10location for flight in 1:F, location in 1:2]
 
@@ -55,61 +58,55 @@ function S(j, t)
     return sectors[sector]
 end
 
-
-function P_s(f, i)
-    sector = P[i,f]
-    return sector_lookup[sector]
-end
-
-function P_a(f, i)
-    airport = P[i, f]
-    return airport_lookup[airport]
-end
-
 function Tjf(f, j)
-    start_time = start_df[f, j]
-    end_time = start_df[f, j] + buffer_time
-    start_time = 1
-    end_time = 24
+    start_time = start_df[j, f]
+    end_time = start_time + buffer_time
     return start_time:end_time
 end
 
+function W(f, t, j)
+    f = convert(Int, f)
+    t = convert(Int, t)
+    j = convert(Int, j)
+
+    return w[P[j, f]][f, t]
+end
 # Declaring variables
-@variable(m, w[1:F, T, 1:J], Bin)
+
+w = Dict(sector => [@variable(m, binary = true) for f in 1:F, t in 1:T] for sector in hcat(sectors_list, airports_list))
 
 # Setting the objective
-@objective(m, Min, sum(((c[f,1]-c[f,2]) * sum(t*(w[f, t, P_a(f, 1)] - w[f, t-1, P_a(f, 1)]) for t in T)) + (c[f, 2] * sum(t*(w[f, t, P_a(f, N[f])] - w[f, t-1, P_a(f, N[f])]) for t in T)) for f in 1:F))
+@objective(m, Min, sum(((c[f,2]-c[f,1]) * sum(t*(W(f, t, 1) - W(f, t-1, 1)) for t in Tjf(f, 1))) + (c[f, 1] * sum(t*(W(f, t, N[f]) - W(f, t-1, N[f])) for t in Tjf(f, N[f]))) for f in 1:F))
+
+
 
 println("Objective good")
 # Adding constraints
 
 # airport and sector capacity constraints
-for k in 1:K, t in T
-    @constraint(m, sum((w[f, t, P_a(f, 1)] - w[f, t-1, P_a(f, 1)]) for f in 1:F) <= D(k, t))
-    @constraint(m, sum((w[f, t, P_a(f, N[f])] - w[f, t-1, P_a(f, N[f])]) for f in 1:F) <= A(k, t))
+for k in 1:K, t in 2:T
+    @constraint(m, sum(W(f, t, 1) - W(f, t-1, 1) for f in 1:F) <= D(k, t))
+    @constraint(m, sum(W(f, t, N[f]) - W(f, t-1, N[f]) for f in 1:F) <= A(k, t))
 end
-for j in 1:J, t in T
-    @constraint(m, sum(sum(w[f, t, P_a(f, i)] - w[f, t, P_a(f, i+1)] for i in 1:N[f]-1) for f in 1:F) <= S(k, t))
+for j in 1:J, t in 2:T
+    @constraint(m, sum(sum(W(f, t, i) - W(f, t, i+1) for i in 1:N[f]-1) for f in 1:F) <= S(j, t))
 end
 
 # connectivity constraints
 for f in 1:F
     for i in 1:N[f]-1
         # didn't include connecting flights
-        for t in Tjf(f, j)
-            @constraint(w[f, t + l[f, P_a(f, i)], P_a(f, i+1)] - w[f, t, P_a(f, i)] <= 0)
+        for t in Tjf(f, i)
+            @constraint(m, W(f, t + l[i, f], i + 1) - W(f, t, i) <= 0)
         end
     end
 
     for j in 1:N[f]
         for t in Tjf(f, j)
-            @constraint(w[f, t, P_s(f, j)] - w[f, t-1, P_s(f, j)])
+            @constraint(m, W(f, t, j) - W(f, t-1, j) >= 0)
         end
     end
 end
-
-# Printing the prepared optimization model
-print(m)
 
 # Solving the optimization problem
 JuMP.optimize!(m)
